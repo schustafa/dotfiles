804164969c0be247957fa7822154037cf0918d05
AJ Schuster <ajschu@gmail.com>
2021-09-28 11:58:31 -0400

    Add some logging
    Borrowed from https://github.com/github/codespaces-dotfiles-template/blob/main/install.sh#L3-L5

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 # Bootstrapped using https://github.com/skalnik/dotfiles/blob/300bb5ce40edf3d7c1bb6780e288c463e0afed81/install.sh
 
+exec > >(tee -i $HOME/dotfiles_install.log)
+exec 2>&1
+set -x
+
 DIR="$(pwd -P $0)"
 
 # Link all linkable files
